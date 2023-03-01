0f264b0e00ff9c050264b587944dd567ef81869b
rakeshMolakala <rakeshmartin999@gmail.com>
2023-03-01 14:37:54 -0500

    a6 sp23 commit3

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -28,7 +28,7 @@ const ExploreComponent = () => {
                 </li>
             </ul>
             <div className="position-relative mb-2">
-                <img src="./images/spacex.png" className="w-100"/>
+                <img src="./images/spacex.png" className="w-100" alt={"#"}/>
                 <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
